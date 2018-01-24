4d14badd9083ff9ca3e6239f35df23dd8c2740d7
ConnerTenn@Desktop <connertenn@gmail.com>
2018-01-24 04:05:11 -0500

    file upload

---
+++ 2D Game/2D Game/GameWind.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace _2D_Game
+{
+    public partial class GameWind : Form
+    {
+        //Main game object for managing things like graphics, ect.
+        private Game game = new Game();
+
+        public GameWind()
+        {
+            InitializeComponent();
+        }
+
+        //Canvas paint function - launches paint functionality
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            game.loadLevel();
+            Graphics g = canvas.CreateGraphics();
+            game.startGraphics(g);
+        }
+
+        //Stops the game before the form is closed,and therefore before any exceptions can occur.
+        private void GameWind_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            game.stopGame();
+        }
+
+        private void GameWind_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+        }
+
+        // Allows the cammand line to be seen during normal execution
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAsAttribute(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+    }
+}
---
+++ 2D Game/2D Game/Level.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Game
+{
+    class Level
+    {
+        private static TextureID[,] blocks = new TextureID[Game.LEVEL_WIDTH, Game.LEVEL_HEIGHT];
+
+        public static TextureID[,] Blocks
+        {
+            get { return blocks; }
+            set { blocks = value; }
+        }
+
+        public static void initLevel()
+        {
+            for (int x = 0; x < Game.LEVEL_WIDTH; x++)
+            {
+                for (int y = 0; y < Game.LEVEL_HEIGHT; y++)
+                {
+                    if (y >= 11)
+                    {
+                        blocks[x, y] = TextureID.dirt;
+                    }
+                    else if (y == 10 && x == 10)
+                    {
+                        blocks[x, y] = TextureID.dirt;
+                    }
+                    else
+                    {
+                        blocks[x, y] = TextureID.air;
+                    }
+                }
+            }
+        }
+    }
+}
