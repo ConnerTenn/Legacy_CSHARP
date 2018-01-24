a0787ef737d37b3fc4adc0034aeb7fa797a5d334
ConnerTenn@Desktop <connertenn@gmail.com>
2018-01-24 04:07:33 -0500

    File Upload

---
+++ CSharpTest/CSharpTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("A={0}", A());
+            Class1 classa = new Class1();
+            Console.WriteLine("classa = {0}", classa.a);
+            Console.ReadLine();
+        }
+        static int A()
+        {
+            return 5;
+        }
+    }
+}
---
+++ ControlStatements/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] ABC = { "A", "B", "C" };
+            string[] DEF = { "D", "E", "F" };
+            Console.WriteLine("ABC:{0}{1}{2}, DEF:{3}{4}{5}", ABC[0], ABC[1], ABC[2], DEF[0], DEF[1], DEF[2]);
+            Console.ReadLine();
+        }
+    }
+}
